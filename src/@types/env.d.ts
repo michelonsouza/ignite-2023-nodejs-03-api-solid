2689428064acb2a502d9f7b88870e111c269eb56
Michelon Souza <michelon.souza@hotmail.com>
2023-10-21 15:30:17 -0300

    feat: set docker-compose en variables

--- src/@types/env.d.ts
+++ src/@types/env.d.ts
@@ -9,6 +9,10 @@ export declare global {
       NODE_ENV: NodeEnv;
       LOGGER?: BooleanType;
       DATABASE_URL: string;
+      POSTGRESQL_USERNAME?: string;
+      POSTGRESQL_PASSWORD?: string;
+      POSTGRESQL_DATABASE?: string;
+      POSTGRESQL_PORT?: string;
     }
   }
 }
